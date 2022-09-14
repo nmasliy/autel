c01269411acb3b3dbaba9f2dea4109702024aef8
Nick <deckards2505@gmail.com>
2022-09-14 19:49:20 +0600

    fixes

--- src/js/components/scroll.js
+++ src/js/components/scroll.js
@@ -1,3 +1,6 @@
 import 'simplebar';
 import SmoothScroll from 'smooth-scroll';
-const scroll = new SmoothScroll('a[href*="#"]');
+const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 500,
+  speedAsDuration: true
+});
